f6b54d6241e70bcbacca32510bf9db3e21438acf
nk-yuta <nk-yuta@iij.ad.jp>
2026-08-04 22:24:23 +0900

    Fix setup_for_ansible.sh

--- setup/lxd_scripts/setup_for_ansible.sh
+++ setup/lxd_scripts/setup_for_ansible.sh
@@ -3,17 +3,19 @@
 BOOTCAMP_ANSIBLE_KEY='<your-key>'
 
 # ユーザ作成（ホームディレクトリ作成、sudoグループ追加）
-sudo adduser ansible-deploy
-sudo usermod -aG sudo ansible-deploy
+echo "Creating ansible-deploy user..."
+adduser --gecos "" --disabled-password ansible-deploy
+usermod -aG sudo ansible-deploy
 
 # sudo パスワード不要にする
 echo 'ansible-deploy ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ansible-deploy
-sudo chmod 440 /etc/sudoers.d/ansible-deploy
+chmod 440 /etc/sudoers.d/ansible-deploy
 
 # 公開鍵登録
-sudo mkdir -p /home/ansible-deploy/.ssh
-sudo chmod 700 /home/ansible-deploy/.ssh
-echo $KEY | sudo tee /home/ansible-deploy/.ssh/authorized_keys
+mkdir -p /home/ansible-deploy/.ssh
+chmod 700 /home/ansible-deploy/.ssh
+echo $BOOTCAMP_ANSIBLE_KEY | sudo tee /home/ansible-deploy/.ssh/authorized_keys
 
-sudo chmod 600 /home/ansible-deploy/.ssh/authorized_keys
-sudo chown -R ansible-deploy:ansible-deploy /home/ansible-deploy/.ssh
+chmod 600 /home/ansible-deploy/.ssh/authorized_keys
+chown -R ansible-deploy:ansible-deploy /home/ansible-deploy/.ssh
+echo "Completed!"
